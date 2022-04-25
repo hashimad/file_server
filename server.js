fd81b2f3ea633954249e5f77a775d05792594e17
Barny victor <victorbarny4@gmail.com>
2022-04-25 11:41:46 +0100

    made some changes in the routes fs file

--- server.js
+++ server.js
@@ -1,55 +1,66 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 const os = require("os");
 
-const host = '127.0.0.1';
+const host = "127.0.0.1";
 const port = 5000;
 
 const server = http.createServer((req, res) => {
-   
-    const urlPath = req.url;
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/html');
-    if(urlPath ==='/'){
-        fs.createReadStream(__dirname+"/pages/index.html").pipe(res);
-    }
-    else if(urlPath === '/about')
-    {
-        fs.createReadStream(__dirname+"/pages/about.html").pipe(res);
-    }
-    else if(urlPath === '/sys')
-    {
-        
-        res.setHeader('Content-Type','text/plain');
-        const data = JSON.stringify({
-            hostname: os.hostname(),
-            platform: os.platform(),
-            architecture: os.arch(),
-            numberOfCPUS: os.cpus() ,
-            networkInterfaces: os.networkInterfaces(),
-            uptime: os.uptime()
-            });
+  const urlPath = req.url;
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  //Index Page
+  if (urlPath === "/") {
+    fs.readFile("pages/index.html", (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        res.write("File not found");
+      } else {
+        res.write(data);
+      } // end of else
+      res.end();
+    });
+  }
+  //About Page
+  else if (urlPath === "/about") {
+    fs.readFile("pages/about.html", (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        res.write("File not found");
+      } else {
+        res.write(data);
+      } // end of else
+      res.end();
+    });
+  }
+  //system info
+  else if (urlPath === "/sys") {
+    res.setHeader("Content-Type", "text/plain");
+    const data = JSON.stringify({
+      hostname: os.hostname(),
+      platform: os.platform(),
+      architecture: os.arch(),
+      numberOfCPUS: os.cpus(),
+      networkInterfaces: os.networkInterfaces(),
+      uptime: os.uptime(),
+    });
 
-         fs.writeFile('osinfo.json', data, (err) => {
-                if (err) 
-                {
-                    res.end('Error Occur');
-                };
-                res.statusCode = 201;
-                res.end('Your OS info has been saved successfully!')
-            });
-        
-    }
-    else
-    {
-        res.statusCode = 404;
-        fs.createReadStream(__dirname+"/pages/404.html").pipe(res);
-    }
-   
-   
+    fs.writeFile("osinfo.json", data, (err) => {
+      if (err) {
+        res.end("Error Occur");
+      }
+      res.statusCode = 201;
+      res.end("Your OS info has been saved successfully!");
+    });
+  }
 
+  //404 Page
+  else {
+    res.statusCode = 404;
+    fs.createReadStream(__dirname + "/pages/404.html").pipe(res);
+  }
 });
 
-server.listen(port,host, () =>{
-    console.log(`Running at ${host}:${port}`)
-})
+server.listen(port, host, () => {
+  console.log(`Server Running at ${host}:${port}`);
+});
